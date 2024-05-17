85a6359c6b6a94a16989ec508a9e98d7231f3a2a
pratham834 <thakkpra@sheridancollege.ca>
2024-05-17 09:57:57 -0400

    git ignore file

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * each student can: say their name (getName)
  * 
  * @author dancye May 7, 2018
- * modified by pratham thakkar 
+ * modified by pratham 
  */
 public class Student 
 {
